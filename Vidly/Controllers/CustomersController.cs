770a7803dd12178e56f8bbba9e84ba85809c23cd
Kamran <skamranmanzoor@yahoo.com>
2016-06-11 16:17:57 +0100

    Changed Customer Name

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "Kamran", Id = 1},
+                new Customer() {Name = "Manzoor", Id = 2}
+            };
+
+            TempData["customers"] = customers;
+
+            ViewBag.Customers = customers;
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            string name = string.Empty;
+
+            if (TempData["customers"] != null)
+            {
+                //var customer = TempData["customers"] as Customer;
+                //var customer = (Object[])TempData["customers"];
+
+            }
+
+            return Content(name);
+        }
+    }
+}
