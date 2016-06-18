c9f55a6e40d699087a1c31faa4b3dcfe3993cfff
Kamran <skamranmanzoor@yahoo.com>
2016-06-18 03:18:46 +0100

    Section4 Customer new-edit-save

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -43,22 +45,51 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes,
                 
             };
 
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                //can also use
+                //TryUpdateModel(customerInDb);
+                
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+
+                // Can also use automapper Mapper.Map(customer,customerInDb);
+                // DTO(Data Transfer Object) is used when using automapping 
+            }
+            
             _context.SaveChanges();
             return RedirectToAction("Index", "Customers");
         }
 
 
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.Single(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm",viewModel);
+        }
     }
 }
